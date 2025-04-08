3302ab5441d1b82f0fc2efa71c22fb38824710e2
Ana <dns80791@gmail.com>
2025-04-08 14:35:32 +0800

    Добавьте файлы проекта.

---
+++ ClientServer_Multithreading/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClientServer_Multithreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
